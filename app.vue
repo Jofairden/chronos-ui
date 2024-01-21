9a81d03e1fd1ed1e580e9d3d5490c4f7ee53752e
Jofairden <Jofairden@users.noreply.github.com>
2024-01-21 22:24:00 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtClientFallback fallback-tag="span" fallback="500">
+  </NuxtClientFallback>
+  <NuxtLayout>
+    <NuxtLoadingIndicator />
+
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+<script setup lang="ts">
+</script>
